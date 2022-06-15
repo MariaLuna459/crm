3a3de7c39c3a095d5aad75f10a8fee52df2330e4
MariaLuna459 <100859442+MariaLuna459@users.noreply.github.com>
2022-06-15 20:01:26 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/app.css">
+    <title>Авторизация</title>
+    <meta name="description" content="">
+</head>
+
+<body>
+    <div class="container">
+        <form>
+            <input type="text" name="login" placeholder="Логин">
+            <input type="password" name="pass" placeholder="Пароль">
+            <button>Вход</button>
+        </form>
+    </div>
+</body>
+
+</html>
